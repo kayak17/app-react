80079fe46e3df006280bf677ff43d9a4f9ea122c
kayak17 <kayak93@list.ru>
2022-01-05 22:25:30 +0300

    Добавлена обработка запроса на login в createAPI

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { HttpCodes } from '~/constants';
+import { AppMessages, HttpCodes } from '~/constants';
 
 const BACKEND_URL = 'http://localhost:3000/';
 const REQUEST_TIMEOUT = 5000;
@@ -10,7 +10,60 @@ export const createAPI = (onUnauthorized) => {
     timeout: REQUEST_TIMEOUT,
   });
 
+  const onRequestSuccess = (config) => {
+    // *** for fake backend only
+    const { data, headers, method, url } = config;
+
+    if (method === 'post' && url === '/login') {
+      headers.auth = {
+        email: data.email,
+        password: data.password,
+        initialRequest: 'login',
+      };
+
+      config.method = 'get';
+      config.url = '/users';
+    }
+    // ***
+
+    return config;
+  };
+
+  const onRequestFail = (err) => {
+    return Promise.reject(err);
+  };
+
   const onResponseSuccess = (response) => {
+    // *** for fake backend only
+    const { config: { headers }, data } = response;
+    const { auth } = headers;
+
+    if (auth && auth.initialRequest === 'login') {
+      const filteredUsers = data.filter((user) => {
+        return user.email === auth.email && user.password === auth.password;
+      });
+
+      if (filteredUsers.length) {
+        const user = filteredUsers[0];
+
+        response.data = {
+          id: user.id,
+          avatar: user.avatar,
+          email: user.email,
+          name: user.name,
+          token: 'fake-jwt-token',
+        };
+      } else {
+        return Promise.reject({
+          message: AppMessages.NO_SUCH_USER,
+          response: {
+            status: '',
+          },
+        });
+      }
+    }
+    // ***
+
     return response;
   };
 
@@ -24,6 +77,7 @@ export const createAPI = (onUnauthorized) => {
     return Promise.reject(err);
   };
 
+  api.interceptors.request.use(onRequestSuccess, onRequestFail);
   api.interceptors.response.use(onResponseSuccess, onResponseFail);
 
   return api;
