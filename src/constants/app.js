a3a3fd402d900890512b977d9d30a31993e5d0e3
kayak17 <kayak93@list.ru>
2022-01-05 22:12:51 +0300

    Добавлены enums AppReducers, AuthStatuses, HttpCodes, ResponseStatusTexts

---
+++ src/constants/app.js
@@ -0,0 +1,17 @@
+export const AppReducers = {
+  USER: 'USER',
+};
+
+export const AuthStatuses = {
+  AUTH: 'AUTH',
+  NO_AUTH: 'NO_AUTH',
+};
+
+export const HttpCodes = {
+  UNAUTHORIZED: 401,
+};
+
+export const ResponseStatusTexts = {
+  CREATED: 'Created',
+  OK: 'OK',
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+export * from './app';
 export * from './markup';
 export * from './messages';
 export * from './modules';
