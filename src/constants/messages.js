9fa3def3ced89fc0aec3ce61b6f17b18e1ca2887
kayak17 <kayak93@list.ru>
2022-01-06 22:39:57 +0300

    Добавлена константа SOMETHING_WENT_WRONG в AppMessages

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,5 @@
 export const AppMessages = {
   DATA_POSTING_ERROR: 'Data posting error',
   NO_SUCH_USER: 'User with such email and password is not found',
+  SOMETHING_WENT_WRONG: 'Something went wrong',
 };
