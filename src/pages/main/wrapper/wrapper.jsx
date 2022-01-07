22b1b004871ca3c2c6faa74032c7f5e53e5ecffb
kayak17 <kayak93@list.ru>
2022-01-07 23:33:11 +0300

    MainLayout подключен в PageMainWrapper

--- src/pages/main/wrapper/wrapper.jsx
+++ src/pages/main/wrapper/wrapper.jsx
@@ -1,11 +1,14 @@
 import { lazy, Suspense } from 'react';
+import MainLayout from '~/layouts/main/main';
 
 const PageMain = lazy(() => import('../content/content'));
 
 const PageMainWrapper = () => {
   return (
     <Suspense fallback={null}>
-      <PageMain />
+      <MainLayout>
+        <PageMain />
+      </MainLayout>
     </Suspense>
   );
 };
