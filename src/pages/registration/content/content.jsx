05716697cc13572d06759081f3c922d79b96674e
kayak17 <kayak93@list.ru>
2022-01-07 23:31:52 +0300

    PageRegistrationContent переименован в PageSignUpContent

--- src/pages/registration/content/content.jsx
+++
@@ -1,7 +0,0 @@
-const PageRegistrationContent = () => {
-  return (
-    <h1>Registration</h1>
-  );
-};
-
-export default PageRegistrationContent;
---
+++ src/pages/sign-up/content/content.jsx
@@ -0,0 +1,7 @@
+const PageSignUpContent = () => {
+  return (
+    <h1>SignUp</h1>
+  );
+};
+
+export default PageSignUpContent;
