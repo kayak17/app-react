d423450dade18b5ebea0049b44c1cd4de49b1f4a
kayak17 <kayak93@list.ru>
2022-01-08 22:25:25 +0300

    Добавлен компонент LogoHeaderInactive

---
+++ src/components/logo/header/inactive/inactive.jsx
@@ -0,0 +1,14 @@
+import { AppTitles } from '~/constants';
+
+const LogoHeaderInactive = () => {
+  return (
+    <a
+      className="pe-none d-flex align-items-center mb-0 me-auto text-dark text-decoration-none"
+      tabIndex="-1"
+    >
+      <span className="fs-4">{AppTitles.LOGO_TITLE}</span>
+    </a>
+  );
+};
+
+export default LogoHeaderInactive;
