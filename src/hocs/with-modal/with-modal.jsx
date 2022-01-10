c8a7e29d8f5f8f756313024af1cbee1f2c78ec7c
kayak17 <kayak93@list.ru>
2022-01-10 23:39:15 +0300

    Добавлен HOC withModal

---
+++ src/hocs/with-modal/with-modal.jsx
@@ -0,0 +1,45 @@
+import { useCallback, useState } from 'react';
+import ModalsManager from '~/components/modals/manager/manager';
+import { ModalIds } from '~/constants';
+
+const withModal = (WrappedComponent) => {
+  const WithModalHOC = (props) => {
+    const [modalOpen, setModalOpen] = useState(ModalIds.NONE);
+
+    const handleOpenModal = useCallback((evt) => {
+      const {
+        target: {
+          dataset: { modal }
+        }
+      } = evt;
+
+      if (modal) {
+        setModalOpen(modal);
+      }
+    }, [setModalOpen]);
+
+    const handleCloseModal = useCallback(() => {
+      setModalOpen(ModalIds.NONE);
+    }, [setModalOpen]);
+
+    return (
+      <div
+        onClick={handleOpenModal}
+      >
+        <WrappedComponent
+          {...props}
+        />
+        <ModalsManager
+          handleClose={handleCloseModal}
+          modal={modalOpen}
+        />
+      </div>
+    );
+  };
+
+  WithModalHOC.displayName = 'WithModalHOC';
+
+  return WithModalHOC;
+};
+
+export default withModal;
