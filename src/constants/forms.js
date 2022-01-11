db7d6d3975cd47fe3e7199c46b1e109cfac3c0ef
kayak17 <kayak93@list.ru>
2022-01-11 20:33:13 +0300

    Добавлены константы для валидации форм и FormValidationMessages

--- src/constants/forms.js
+++ src/constants/forms.js
@@ -1,3 +1,6 @@
+export const FORM_FORBIDDEN_SYMBOLS = '< > /';
+export const FORM_FORBIDDEN_SYMBOLS_REGEX = /^[^<>/]*$/;
+
 export const FORM_INITIAL_ERROR = undefined;
 export const FORM_INITIAL_SUCCESS = undefined;
 
@@ -13,4 +16,12 @@ export const FormSRTitles = {
   NAME: 'Name',
   PASSWORD: 'Password',
   PASSWORD_CONFIRM: 'Password confirmation',
+};
+
+export const FormValidationMessages = {
+  EMAIL_INVALID: 'invalid email',
+  FORBIDDEN_SYMBOLS: `don't use forbidden symbols: ${FORM_FORBIDDEN_SYMBOLS}`,
+  PASSWORD_INVALID: 'invalid password',
+  PASSWORD_CONFIRM_REQUIRED: 'password confirmation is required',
+  PASSWORDS_DO_NOT_MATCH: 'passwords must match',
 };
