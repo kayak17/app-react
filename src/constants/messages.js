d5e0d80dab8c663b7404ccc56c094666c4180c5b
kayak17 <kayak93@list.ru>
2022-01-11 20:24:42 +0300

    Добавлены константы DONT_HAVE_ACCOUNT и LOGIN_SUCCESS в AppMessages

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,7 @@
 export const AppMessages = {
   DATA_POSTING_ERROR: 'Data posting error',
+  DONT_HAVE_ACCOUNT: ' if you don\'t have an account yet',
+  LOGIN_SUCCESS: 'You successfully signed in',
   NO_SUCH_USER: 'User with such email and password is not found',
   SOMETHING_WENT_WRONG: 'Something went wrong',
 };
