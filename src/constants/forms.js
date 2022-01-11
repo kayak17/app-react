7f8fc02c99837753272db9373ff849454b054c69
kayak17 <kayak93@list.ru>
2022-01-11 20:25:55 +0300

    Добавлены константы FORM_INITIAL_ERROR, FORM_INITIAL_SUCCESS и enums FormInputPlaceholders, FormSRTitles

---
+++ src/constants/forms.js
@@ -0,0 +1,16 @@
+export const FORM_INITIAL_ERROR = undefined;
+export const FORM_INITIAL_SUCCESS = undefined;
+
+export const FormInputPlaceholders = {
+  EMAIL: 'E-mail',
+  NAME: 'Name',
+  PASSWORD: 'Password',
+  PASSWORD_CONFIRM: 'Password confirmation',
+};
+
+export const FormSRTitles = {
+  EMAIL: 'E-mail',
+  NAME: 'Name',
+  PASSWORD: 'Password',
+  PASSWORD_CONFIRM: 'Password confirmation',
+};
