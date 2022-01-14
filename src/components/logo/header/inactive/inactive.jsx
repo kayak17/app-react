ad08302de8871d3e6e8ac9d18a9423d8ecbfb331
kayak17 <kayak93@list.ru>
2022-01-14 23:37:42 +0300

    В LogoHeaderInactive добавлен css-класс app-subtitle для логотипа

--- src/components/logo/header/inactive/inactive.jsx
+++ src/components/logo/header/inactive/inactive.jsx
@@ -3,7 +3,7 @@ import { AppTitles } from '~/constants';
 const LogoHeaderInactive = () => {
   return (
     <a
-      className="pe-none d-flex align-items-center mb-0 me-auto text-dark text-decoration-none"
+      className="app-subtitle pe-none d-flex align-items-center mb-0 me-auto text-dark text-decoration-none"
       tabIndex="-1"
     >
       <span className="fs-4">{AppTitles.LOGO_TITLE}</span>
