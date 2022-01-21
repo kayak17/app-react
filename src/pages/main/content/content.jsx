35f6a36c37f986867749daaec0ef3fd0078706cc
kayak17 <kayak93@list.ru>
2022-01-21 21:45:27 +0300

    Правки формирования разметки PageMainContent

--- src/pages/main/content/content.jsx
+++ src/pages/main/content/content.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import CitiesList from '~/components/cities/list/list';
 import CitiesListEmpty from '~/components/cities/list-empty/list-empty';
 import PlacesWrapper from '~/components/places/wrapper/wrapper';
-import { AppSRTitles } from '~/constants';
+import { AppSRTitles, AppMessages } from '~/constants';
 
 const PageMainContent = ({
   cities,
@@ -11,37 +11,50 @@ const PageMainContent = ({
   isCitiesLoaded,
   isOffersLoading,
   isOffersError,
+  isOffersLoaded,
 }) => {
+  const throwErrorToBoundary = () => {
+    throw new Error(AppMessages.DATA_LOADING_ERROR);
+  };
+
+  const getCitiesMarkup = () => {
+    if (isCitiesLoaded) {
+      return <CitiesList cities={cities} />;
+    } else {
+      return <CitiesListEmpty />;
+    }
+  };
+
+  const getPlacesMarkup = () => {
+    if (!isCitiesError && isOffersLoading) {
+      return null;
+    } else if (isCitiesError || isOffersError) {
+      throwErrorToBoundary();
+    } else if (isOffersLoaded) {
+      return (
+        <div className="row mx-0">
+          <h1 className="visually-hidden">
+            {AppSRTitles.MAIN_PAGE_PLACES}
+          </h1>
+          <section className="col-6 text-center">
+            <PlacesWrapper
+              offersReducer={offersReducer}
+            />
+          </section>
+          <section className="col-6 text-center bg-light">
+
+          </section>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
 
   return (
     <>
-      {isCitiesLoaded ? (
-        <CitiesList cities={cities} />
-      ) : (
-        <CitiesListEmpty />
-      )}
-
-      {!isCitiesError && isOffersLoading ? (
-        null
-      ) :
-        isCitiesError || isOffersError ? (
-          null
-        ) : (
-          <div className="row mx-0">
-            <h1 className="visually-hidden">
-              {AppSRTitles.MAIN_PAGE_PLACES}
-            </h1>
-            <section className="col-6 text-center">
-              <PlacesWrapper
-                offersReducer={offersReducer}
-              />
-            </section>
-            <section className="col-6 text-center bg-light">
-
-            </section>
-          </div>
-        )
-      }
+      {getCitiesMarkup()}
+      {getPlacesMarkup()}
     </>
   );
 };
@@ -53,6 +66,7 @@ PageMainContent.propTypes = {
   isCitiesLoaded: PropTypes.bool.isRequired,
   isOffersLoading: PropTypes.bool.isRequired,
   isOffersError: PropTypes.bool.isRequired,
+  isOffersLoaded: PropTypes.bool.isRequired,
 };
 
 export default PageMainContent;
