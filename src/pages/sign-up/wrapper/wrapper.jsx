ac08ab94db577496d5fa4791f54bea2f1736f2d3
kayak17 <kayak93@list.ru>
2022-01-21 21:53:26 +0300

    Рефакторинг PageSignUpWrapper

--- src/pages/sign-up/wrapper/wrapper.jsx
+++ src/pages/sign-up/wrapper/wrapper.jsx
@@ -1,17 +1,22 @@
 import { lazy } from 'react';
-import PageWrapper from '~/components/page-wrapper/page-wrapper';
+import PropTypes from 'prop-types';
+import ErrorBoundary from '~/components/error-boundary/error-boundary';
 import MainLayout from '~/layouts/main/main';
 
 const PageSignUp = lazy(() => import('../content/content'));
 
-const PageSignUpWrapper = () => {
+const PageSignUpWrapper = ({ setIsLoading }) => {
   return (
-    <PageWrapper
-      isSpinner={false}
-      Layout={MainLayout}
-      PageContent={PageSignUp}
-    />
+    <MainLayout>
+      <ErrorBoundary setIsLoading={setIsLoading}>
+        <PageSignUp />
+      </ErrorBoundary>
+    </MainLayout>
   );
+};
+
+PageSignUpWrapper.propTypes = {
+  setIsLoading: PropTypes.func.isRequired,
 };
 
 export default PageSignUpWrapper;
