435a0c2f8f419c957e0c61dfafcfaaefd175b39f
kayak17 <kayak93@list.ru>
2022-01-21 21:52:57 +0300

    Рефакторинг PageNotFoundWrapper

--- src/pages/not-found/wrapper/wrapper.jsx
+++ src/pages/not-found/wrapper/wrapper.jsx
@@ -1,17 +1,22 @@
 import { lazy } from 'react';
-import PageWrapper from '~/components/page-wrapper/page-wrapper';
+import PropTypes from 'prop-types';
+import ErrorBoundary from '~/components/error-boundary/error-boundary';
 import MainLayout from '~/layouts/main/main';
 
 const PageNotFound = lazy(() => import('../content/content'));
 
-const PageNotFoundWrapper = () => {
+const PageNotFoundWrapper = ({ setIsLoading }) => {
   return (
-    <PageWrapper
-      isSpinner={false}
-      Layout={MainLayout}
-      PageContent={PageNotFound}
-    />
+    <MainLayout>
+      <ErrorBoundary setIsLoading={setIsLoading}>
+        <PageNotFound />
+      </ErrorBoundary>
+    </MainLayout>
   );
+};
+
+PageNotFoundWrapper.propTypes = {
+  setIsLoading: PropTypes.func.isRequired,
 };
 
 export default PageNotFoundWrapper;
