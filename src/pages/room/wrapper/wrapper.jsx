38b70f08665515a0d1652557438b279187bc291d
kayak17 <kayak93@list.ru>
2022-01-21 21:53:13 +0300

    Рефакторинг PageRoomWrapper

--- src/pages/room/wrapper/wrapper.jsx
+++ src/pages/room/wrapper/wrapper.jsx
@@ -1,17 +1,22 @@
 import { lazy } from 'react';
-import PageWrapper from '~/components/page-wrapper/page-wrapper';
+import PropTypes from 'prop-types';
+import ErrorBoundary from '~/components/error-boundary/error-boundary';
 import CommonLayout from '~/layouts/common/common';
 
 const PageRoom = lazy(() => import('../content/content'));
 
-const PageRoomWrapper = () => {
+const PageRoomWrapper = ({ setIsLoading }) => {
   return (
-    <PageWrapper
-      isSpinner={false}
-      Layout={CommonLayout}
-      PageContent={PageRoom}
-    />
+    <CommonLayout>
+      <ErrorBoundary setIsLoading={setIsLoading}>
+        <PageRoom />
+      </ErrorBoundary>
+    </CommonLayout>
   );
+};
+
+PageRoomWrapper.propTypes = {
+  setIsLoading: PropTypes.func.isRequired,
 };
 
 export default PageRoomWrapper;
