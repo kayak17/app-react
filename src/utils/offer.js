002192a152e0306b4a7fb726fa09cbcec2fd955f
kayak17 <kayak93@list.ru>
2022-01-22 23:05:12 +0300

    Добавлена функция getOfferPriceTitle в utils

--- src/utils/offer.js
+++ src/utils/offer.js
@@ -3,6 +3,10 @@ import {
   AppQueryParamsLeading,
 } from '~/constants';
 
+export const getOfferPriceTitle = (price) => (
+  `€ ${price} / night`
+);
+
 export const getOffersURL = (cityId) => (
   `${APIRoutes.OFFERS}${AppQueryParamsLeading.CITY_ID}${cityId}&_limit=7`
 );
