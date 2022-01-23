ef9fbaf708de26bc38b64da06260f5f7f2f8f051
kayak17 <kayak93@list.ru>
2022-01-23 23:40:14 +0300

    Правки разметки OfferCardPlaceholder

--- src/components/offer/card-placeholder/card-placeholder.jsx
+++ src/components/offer/card-placeholder/card-placeholder.jsx
@@ -4,20 +4,23 @@ const OfferCardPlaceholder = () => {
   return (
     <li className="col mb-3" aria-hidden="true">
       <article className="card border-light text-start">
-        <div className="row g-0">
-          <div className="col-6">
-            <div className="placeholder bg-secondary h-100 rounded-start offer-card-img"></div>
+        <div className="d-flex">
+          <div className="offer-card-img-wrapper">
+            <div className="h-100 bg-secondary rounded offer-card-img placeholder"></div>
           </div>
           <div className="col-6">
             <div className="card-body px-3 py-1">
-              <div className="card-title mb-1 placeholder-glow">
-                <span className="offer-card-price placeholder bg-secondary col-6"></span>
+              <div className="card-text mb-1 placeholder-glow">
+                <span className="col-5 bg-secondary fs-5-4 placeholder"></span>
               </div>
-              <h6 className="card-text mb-1 placeholder-glow">
-                <span className="offer-card-title placeholder bg-secondary col-8"></span>
+              <div className="card-text mb-1 placeholder-glow">
+                <span className="col-4 bg-secondary fs-5-4 placeholder"></span>
+              </div>
+              <h6 className="card-title mb-1 placeholder-glow">
+                <span className="col-8 bg-secondary placeholder fs-5-3"></span>
               </h6>
               <p className="card-text placeholder-glow">
-                <small className="fs-6 placeholder bg-secondary col-4"></small>
+                <small className="col-4 bg-secondary fs-6-1 placeholder"></small>
               </p>
             </div>
           </div>
