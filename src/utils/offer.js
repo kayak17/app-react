8394004e4e6232d7de8db3a76257749e1b990183
kayak17 <kayak93@list.ru>
2022-01-23 23:39:17 +0300

    Вместо функции getOfferPriceTitle добавлены функции getOfferCurrency, getOfferPricePeriod

--- src/utils/offer.js
+++ src/utils/offer.js
@@ -3,9 +3,9 @@ import {
   AppQueryParamsLeading,
 } from '~/constants';
 
-export const getOfferPriceTitle = (price) => (
-  `€ ${price} / night`
-);
+export const getOfferCurrency = () => '€';
+
+export const getOfferPricePeriod = () => '/ night';
 
 export const getOffersURL = (cityId) => (
   `${APIRoutes.OFFERS}${AppQueryParamsLeading.CITY_ID}${cityId}&_limit=7`
