c707781a46abe555efb7675b02a921d81d93c404
kayak17 <kayak93@list.ru>
2022-01-24 23:18:11 +0300

    Добавлены actions setOffersListType и setSortingType

--- src/modules/main/actions.js
+++ src/modules/main/actions.js
@@ -1,3 +1,5 @@
 import { createAction } from 'redux-actions';
 
 export const setActiveCity = createAction('SET_ACTIVE_CITY');
+export const setOffersListType = createAction('SET_OFFERS_LIST_TYPE');
+export const setSortingType = createAction('SET_SORTING_TYPE');
