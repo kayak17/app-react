66b33ce4e0fd43f6e41dbfe79d838e09128efee7
kayak17 <kayak93@list.ru>
2022-01-24 23:41:29 +0300

    Доработки OfferCard

--- src/components/offer/card/card.jsx
+++ src/components/offer/card/card.jsx
@@ -1,7 +1,12 @@
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import RatingStars from '~/components/rating/stars/stars';
 import {
   AppRoutes,
+  OfferClasses,
+  OfferImgShapes,
   OfferTitles,
+  RatingTypes,
 } from '~/constants';
 import {
   offerPropTypes,
@@ -14,55 +19,62 @@ import '../offer.less';
 
 const OfferCard = ({
   offer,
+  offerType,
 }) => {
   const offerId = offer.id;
   const offerLink = `${AppRoutes.OFFER}?id=${offerId}`;
 
   return (
-    <li className="col mb-3">
+    <li className={`${OfferClasses[offerType]['li']} mb-3`}>
       <article className="card border-light text-start">
-        <div className="d-flex">
-          <div className="offer-card-img-wrapper">
-            <div className="position-relative bg-light rounded-start">
+        <div className={`${OfferClasses[offerType]['container']} d-flex`}>
+          <div>
+            <div className="position-relative bg-light rounded">
               {offer.wifi && (
                 <span className="position-absolute top-0 start-0 badge bg-primary app-skewed-neg-15 offer-card-badge">
                   <span>{OfferTitles.FREE_WI_FI}</span>
                 </span>
               )}
               <NavLink
-                className="d-flex justify-content-center align-items-center w-100 h-100"
+                className="d-flex justify-content-center align-items-center"
                 to={offerLink}
               >
                 <img
-                  className="img-fluid rounded offer-card-img"
-                  src={offer.image}
+                  className={`${OfferClasses[offerType]['img']} img-fluid rounded`}
                   alt={OfferTitles.PLACE_IMAGE}
+                  width={OfferImgShapes[offerType].width}
+                  height={OfferImgShapes[offerType].height}
+                  src={offer.image}
                 />
               </NavLink>
             </div>
           </div>
-          <div>
-            <div className="card-body px-3 py-1">
-              <div className="card-text mb-1">
-                <b className="fs-5-4">
-                  {getOfferCurrency()}&nbsp;{offer.price}
-                </b>
-                <span className="fs-6-3">
-                  &nbsp;{getOfferPricePeriod()}
-                </span>
-              </div>
-              <h6 className="card-title mb-1">
-                <NavLink
-                  className="text-dark text-decoration-none app-link-opacity app-subtitle fs-5-3"
-                  to={offerLink}
-                >
-                  {offer.title}
-                </NavLink>
-              </h6>
-              <p className="card-text">
-                <small className="text-muted fs-6-1">{offer.type}</small>
-              </p>
+          <div className={`${OfferClasses[offerType]['info']} card-body py-1`}>
+            <div className="card-text mb-1">
+              <b className="fs-5-4">
+                {getOfferCurrency()}&nbsp;{offer.price}
+              </b>
+              <span className="fs-6-3">
+                &nbsp;{getOfferPricePeriod()}
+              </span>
             </div>
+            <div className="card-text mb-1">
+              <RatingStars
+                rating={offer.rating}
+                ratingType={RatingTypes.CARD}
+              />
+            </div>
+            <h6 className="card-title mb-1">
+              <NavLink
+                className="text-dark text-decoration-none app-hover-opacity app-trasition app-subtitle fs-5-3"
+                to={offerLink}
+              >
+                {offer.title}
+              </NavLink>
+            </h6>
+            <p className="card-text">
+              <small className="text-muted fs-6-2">{offer.type}</small>
+            </p>
           </div>
         </div>
       </article>
@@ -72,6 +84,7 @@ const OfferCard = ({
 
 OfferCard.propTypes = {
   offer: offerPropTypes,
+  offerType: PropTypes.string.isRequired,
 };
 
 export default OfferCard;
