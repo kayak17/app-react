4fa073d34362d22427946844deb32589f1554d08
kayak17 <kayak93@list.ru>
2022-01-24 23:19:22 +0300

    В InitialModulesValues добавлены константы OFFERS_LIST_TYPE и SORTING_TYPE

--- src/constants/modules.js
+++ src/constants/modules.js
@@ -1,5 +1,10 @@
+import { OfferStyleTypes } from './offers';
+import { SortingTypeNames } from './sorting';
+
 export const InitialModulesValues = {
   ACTIVE_CITY: {},
   AUTH_INFO: null,
   ERROR: null,
+  OFFERS_LIST_TYPE: OfferStyleTypes.ROW,
+  SORTING_TYPE: SortingTypeNames.POPULAR,
 };
