96e32e776717586e0c8aac6e623dfe8814954d63
kayak17 <kayak93@list.ru>
2022-01-24 23:41:49 +0300

    Добавлены enum OfferClasses и OfferImgShapes

--- src/constants/offers.js
+++ src/constants/offers.js
@@ -1,6 +1,42 @@
 export const OFFER_RATING_STAR_WIDTH = 20;
 export const OFFERS_LIST_TYPE_ARIA_LABEL = 'offers list style change';
 
+export const OfferClasses = {
+  MAIN: {
+    li: 'col',
+    container: '',
+    img: 'offer-card-img',
+    info: '',
+  },
+  ROOM: {
+    li: 'col-6 offer-card-column',
+    container: 'flex-column',
+    img: 'offer-card-img',
+    info: 'px-2',
+  },
+  FAVORITE: {
+    li: 'col',
+    container: '',
+    img: 'offer-card-img-sm',
+    info: '',
+  },
+};
+
+export const OfferImgShapes = {
+  MAIN: {
+    width: 260,
+    height: 200,
+  },
+  ROOM: {
+    width: 260,
+    height: 200,
+  },
+  FAVORITE: {
+    width: 156,
+    height: 120,
+  },
+};
+
 export const OfferTitles = {
   FREE_WI_FI: 'Free Wi-Fi',
   NO_PLACES_TO_STAY_IN: 'No places to stay available at the moment in ',
