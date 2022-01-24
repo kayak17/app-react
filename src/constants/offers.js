4ef8088f8e1fa9f43850e3c883629ef6a5cc9499
kayak17 <kayak93@list.ru>
2022-01-24 23:34:34 +0300

    Добавлены константы OFFER_RATING_STAR_WIDTH и OFFERS_LIST_TYPE_ARIA_LABEL

--- src/constants/offers.js
+++ src/constants/offers.js
@@ -1,3 +1,6 @@
+export const OFFER_RATING_STAR_WIDTH = 20;
+export const OFFERS_LIST_TYPE_ARIA_LABEL = 'offers list style change';
+
 export const OfferTitles = {
   FREE_WI_FI: 'Free Wi-Fi',
   NO_PLACES_TO_STAY_IN: 'No places to stay available at the moment in ',
