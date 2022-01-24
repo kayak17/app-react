670e62ab0d80cb6c85e8bf9ed7a9864dda5bc6ae
kayak17 <kayak93@list.ru>
2022-01-24 23:15:28 +0300

    Убран лишний код из PageLoginWrapper

--- src/pages/login/wrapper/wrapper.jsx
+++ src/pages/login/wrapper/wrapper.jsx
@@ -1,22 +1,17 @@
 import { lazy } from 'react';
-import PropTypes from 'prop-types';
 import ErrorBoundary from '~/components/error-boundary/error-boundary';
 import MainLayout from '~/layouts/main/main';
 
 const PageLogin = lazy(() => import('../content/content'));
 
-const PageLoginWrapper = ({ setIsLoading }) => {
+const PageLoginWrapper = () => {
   return (
     <MainLayout>
-      <ErrorBoundary setIsLoading={setIsLoading}>
+      <ErrorBoundary>
         <PageLogin />
       </ErrorBoundary>
     </MainLayout>
   );
-};
-
-PageLoginWrapper.propTypes = {
-  setIsLoading: PropTypes.func.isRequired,
 };
 
 export default PageLoginWrapper;
