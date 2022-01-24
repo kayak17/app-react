d1a1d2ae30b130aa6e9d3c5157b07db59b624d23
kayak17 <kayak93@list.ru>
2022-01-24 23:15:38 +0300

    Убран лишний код из PageNotFoundWrapper

--- src/pages/not-found/wrapper/wrapper.jsx
+++ src/pages/not-found/wrapper/wrapper.jsx
@@ -1,22 +1,17 @@
 import { lazy } from 'react';
-import PropTypes from 'prop-types';
 import ErrorBoundary from '~/components/error-boundary/error-boundary';
 import MainLayout from '~/layouts/main/main';
 
 const PageNotFound = lazy(() => import('../content/content'));
 
-const PageNotFoundWrapper = ({ setIsLoading }) => {
+const PageNotFoundWrapper = () => {
   return (
     <MainLayout>
-      <ErrorBoundary setIsLoading={setIsLoading}>
+      <ErrorBoundary>
         <PageNotFound />
       </ErrorBoundary>
     </MainLayout>
   );
-};
-
-PageNotFoundWrapper.propTypes = {
-  setIsLoading: PropTypes.func.isRequired,
 };
 
 export default PageNotFoundWrapper;
