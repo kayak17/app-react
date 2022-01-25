6dd04b0e98d0b696157c987d53f0495421f6b159
kayak17 <kayak93@list.ru>
2022-01-25 20:32:15 +0300

    Правки разметки PlacesWrapper

--- src/components/places/wrapper/wrapper.jsx
+++ src/components/places/wrapper/wrapper.jsx
@@ -3,23 +3,21 @@ import OffersList from '~/components/offer/list/list';
 import FormFilters from '../form-filters/form-filters';
 import { getOffersListType } from '~/modules/main';
 import {
-  AppSRTitles,
   InitialModulesValues,
   OfferClassesTypes,
   OfferTitles,
 } from '~/constants';
 import { offersReducerPropTypes } from '~/prop-types';
-import '../places.less';
 
 const PlacesWrapper = ({ offersReducer }) => {
   const activeCityName = offersReducer.activeCityName;
   const totalCount = offersReducer.totalCount;
-  const data = offersReducer.data;
+  const offers = offersReducer.data;
   const offersListType = useSelector(getOffersListType) ||
     InitialModulesValues.OFFERS_LIST_TYPE;
 
   const getTitle = () => {
-    if (data.length) {
+    if (offers.length) {
       return `${totalCount}${OfferTitles.PLACES_TO_STAY_IN}${activeCityName}`;
     }
 
@@ -27,29 +25,21 @@ const PlacesWrapper = ({ offersReducer }) => {
   };
 
   return (
-    <div className="row mx-0 mb-3">
-      <h1 className="visually-hidden">
-        {AppSRTitles.MAIN_PAGE_PLACES}
-      </h1>
-      <section className="col-6 text-center overflow-auto places-container">
-        <h2 className="px-5 my-3 app-subtitle">
-          {getTitle()}
-        </h2>
-        {data.length ? (
-          <>
-            <FormFilters />
-            <OffersList
-              offers={data}
-              offerType={OfferClassesTypes[offersListType]}
-            />
-          </>
-        ) : (
-          null
-        )}
-      </section>
-      <section className="col-6 bg-light text-center places-map-container">
-
-      </section>
+    <div className="text-center">
+      <h2 className="px-5 my-3 app-subtitle">
+        {getTitle()}
+      </h2>
+      {offers.length ? (
+        <>
+          <FormFilters />
+          <OffersList
+            offers={offers}
+            offerType={OfferClassesTypes[offersListType]}
+          />
+        </>
+      ) : (
+        null
+      )}
     </div>
   );
 };
