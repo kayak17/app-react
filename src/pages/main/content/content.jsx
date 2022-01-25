ad2e83c62c727138b23a58038733ab03eda8c334
kayak17 <kayak93@list.ru>
2022-01-25 20:31:39 +0300

    Правки разметки и подключение OffersMap в PageMainContent

--- src/pages/main/content/content.jsx
+++ src/pages/main/content/content.jsx
@@ -2,13 +2,17 @@ import { memo } from 'react';
 import PropTypes from 'prop-types';
 import CitiesList from '~/components/cities/list/list';
 import CitiesListPlaceholder from '~/components/cities/list-placeholder/list-placeholder';
+import OffersMap from '~/components/offer/map/map';
 import PlacesWrapper from '~/components/places/wrapper/wrapper';
 import PlacesWrapperPlaceholder from '~/components/places/wrapper-placeholder/wrapper-placeholder';
-import { citiesPropTypes, offersReducerPropTypes } from '~/prop-types';
+import { AppSRTitles } from '~/constants';
+import { citiesPropTypes, offersPropTypes, offersReducerPropTypes } from '~/prop-types';
 import { throwErrorToBoundary } from '~/utils';
+import './content.less';
 
 const PageMainContent = ({
   cities,
+  offersMap,
   offersReducer,
   isCitiesError,
   isCitiesLoaded,
@@ -45,13 +49,26 @@ const PageMainContent = ({
   return (
     <>
       {getCitiesMarkup()}
-      {getPlacesMarkup()}
+      <div className="row mx-0 mb-3">
+        <h1 className="visually-hidden">
+          {AppSRTitles.MAIN_PAGE_PLACES}
+        </h1>
+        <section className="col-6 overflow-auto main-places-container">
+          {getPlacesMarkup()}
+        </section>
+        <section className="col-6 g-0 main-map-container">
+          <OffersMap
+            offers={offersMap}
+          />
+        </section>
+      </div>
     </>
   );
 };
 
 PageMainContent.propTypes = {
   cities: citiesPropTypes,
+  offersMap: offersPropTypes,
   offersReducer: offersReducerPropTypes,
   isCitiesError: PropTypes.bool.isRequired,
   isCitiesLoaded: PropTypes.bool.isRequired,
