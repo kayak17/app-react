5baa40e2218dcfc840b49900d082ac984ea009f9
kayak17 <kayak93@list.ru>
2022-01-25 20:32:28 +0300

    Правки разметки PlacesWrapperPlaceholder

--- src/components/places/wrapper-placeholder/wrapper-placeholder.jsx
+++ src/components/places/wrapper-placeholder/wrapper-placeholder.jsx
@@ -7,7 +7,6 @@ import {
   OfferClassesTypes,
 } from '~/constants';
 import { getEmptyArray } from '~/utils';
-import '../places.less';
 
 const PlacesWrapperPlaceholder = () => {
   const dataArray = getEmptyArray(OFFERS_LIMIT_PER_PAGE);
@@ -15,18 +14,16 @@ const PlacesWrapperPlaceholder = () => {
     InitialModulesValues.OFFERS_LIST_TYPE;
 
   return (
-    <div className="row mt-1 mb-3 mx-0" aria-hidden="true">
-      <section className="col-6 text-center overflow-auto places-container placeholder-glow">
-        <h2 className="col-6 my-3 bg-secondary app-subtitle placeholder"></h2>
-        <div className="col-12 mt-1 mb-3 bg-secondary fs-5-2 app-subtitle placeholder"></div>
-        <OffersListPlaceholder
-          offers={dataArray}
-          offerType={OfferClassesTypes[offersListType]}
-        />
-      </section>
-      <section className="col-6 bg-light text-center places-map-container">
-
-      </section>
+    <div
+      className="mt-1 text-center placeholder-glow"
+      aria-hidden="true"
+    >
+      <h2 className="col-6 my-3 bg-secondary app-subtitle placeholder"></h2>
+      <div className="col-12 mt-1 mb-3 bg-secondary fs-5-2 app-subtitle placeholder"></div>
+      <OffersListPlaceholder
+        offers={dataArray}
+        offerType={OfferClassesTypes[offersListType]}
+      />
     </div>
   );
 };
