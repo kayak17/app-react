6b33871a74c7c967dbd1dfe68596e7ebd6085904
kayak17 <kayak93@list.ru>
2022-01-25 21:08:49 +0300

    Добавлен компонент PlacesWrapper на основе getPlacesMarkup из PageMainContent

---
+++ src/components/places/wrapper/wrapper.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import PlacesContent from '~/components/places/content/content';
+import PlacesContentPlaceholder from '~/components/places/content-placeholder/content-placeholder';
+import { getOffersListType } from '~/modules/main';
+import { InitialModulesValues } from '~/constants';
+import { offersReducerPropTypes } from '~/prop-types';
+import { throwErrorToBoundary } from '~/utils';
+
+const PlacesWrapper = ({
+  offersReducer,
+  isCitiesError,
+  isOffersError,
+  isOffersLoading,
+  isOffersLoaded,
+}) => {
+  const offersListType = useSelector(getOffersListType) ||
+    InitialModulesValues.OFFERS_LIST_TYPE;
+
+  if (!isCitiesError && isOffersLoading) {
+    return (
+      <PlacesContentPlaceholder
+        offersListType={offersListType}
+      />
+    );
+  } else if (isCitiesError || isOffersError) {
+    throwErrorToBoundary();
+  } else if (isOffersLoaded) {
+    return (
+      <PlacesContent
+        offersReducer={offersReducer}
+        offersListType={offersListType}
+      />
+    );
+  } else {
+    return null;
+  }
+};
+
+PlacesWrapper.propTypes = {
+  offersReducer: offersReducerPropTypes,
+  isCitiesError: PropTypes.bool.isRequired,
+  isOffersError: PropTypes.bool.isRequired,
+  isOffersLoading: PropTypes.bool.isRequired,
+  isOffersLoaded: PropTypes.bool.isRequired,
+};
+
+export default PlacesWrapper;
