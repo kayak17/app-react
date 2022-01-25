b258a5d5c8d80e38f1a5044ad4a57cdcf48f6ed3
kayak17 <kayak93@list.ru>
2022-01-25 20:23:58 +0300

    В rootReducer подключен offersMap reducer

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { AppReducers } from '~/constants';
 import main from './main';
+import offersMap from './offers-map';
 import process from './process';
 import user from './user';
 
 const rootReducer = combineReducers({
   [AppReducers.MAIN]: main,
+  [AppReducers.OFFERS_MAP]: offersMap,
   [AppReducers.PROCESS]: process,
   [AppReducers.USER]: user,
 });
