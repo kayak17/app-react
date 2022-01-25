3827334e82a2c26dae54543d6af3fe4a0735fd08
kayak17 <kayak93@list.ru>
2022-01-25 20:27:40 +0300

    Добавлены константы для OffersMap в constants/offers-map

---
+++ src/constants/offers-map.js
@@ -0,0 +1,8 @@
+export const MAP_CENTER_DEFAULT = [48.85661, 2.351499];
+export const MAP_ID = 'map';
+export const MAP_PIN_SIZE = [30, 30];
+export const MAP_PIN_URL = 'img/pin.svg';
+export const MAP_PIN_ACTIVE_URL = 'img/pin-active.svg';
+export const MAP_TILE_LAYER = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
+export const MAP_TILE_LAYER_ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
+export const MAP_ZOOM_DEFAULT = 13;
