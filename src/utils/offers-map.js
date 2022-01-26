557965b50bc9580d857ff42c5e6e6ff1a8259a01
kayak17 <kayak93@list.ru>
2022-01-26 17:32:43 +0300

    Добавлена функция getMapTooltipMarkup в utils

--- src/utils/offers-map.js
+++ src/utils/offers-map.js
@@ -1,7 +1,28 @@
 import {
   APIRoutes,
   AppQueryParamsLeading,
+  OfferSrTitles,
 } from '~/constants';
+import {
+  getOfferCurrency,
+  getOfferPricePeriod,
+  getRatingStarsWidth,
+} from '~/utils';
+import '~/components/rating/stars/stars.less';
+
+export const getMapTooltipMarkup = ({ price, rating, title, type: flatType }) => {
+  return `
+      <b>${title}</b><br />
+      <span>${getOfferCurrency()} ${price} ${getOfferPricePeriod()}</span>,
+      <span>${flatType}</span><br />
+      <span class="offer-card-rating rating justify-content-center">
+        <span class="offer-card-stars rating-stars">
+          <span style="width: ${getRatingStarsWidth(rating)}%;" />
+          <span className="visually-hidden">${OfferSrTitles.RATING}</span>
+        </span>
+      </span>
+    `;
+};
 
 export const getOffersMapURL = (cityId) => (
   `${APIRoutes.OFFERS}${AppQueryParamsLeading.CITY_ID}${cityId}`
