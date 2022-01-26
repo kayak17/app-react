aef37adb3b1f0d4aade89e986403565a5353dfa0
kayak17 <kayak93@list.ru>
2022-01-26 21:38:21 +0300

    Подключен persistReducer в modules/index

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
-import { AppReducers } from '~/constants';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import main from './main';
 import offersMap from './offers-map';
 import process from './process';
 import user from './user';
+import { AppReducers, APP_LOCAL_STORAGE_NAME } from '~/constants';
+
+const persistConfig = {
+  key: APP_LOCAL_STORAGE_NAME,
+  storage,
+  whitelist: [
+    AppReducers.MAIN,
+    AppReducers.USER,
+  ],
+};
 
 const rootReducer = combineReducers({
   [AppReducers.MAIN]: main,
@@ -12,4 +23,4 @@ const rootReducer = combineReducers({
   [AppReducers.USER]: user,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
