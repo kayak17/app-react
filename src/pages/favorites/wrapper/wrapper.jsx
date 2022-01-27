db0564a031c56b36451e4a51b254052bcbfb4f58
kayak17 <kayak93@list.ru>
2022-01-27 22:03:25 +0300

    Убран лишний код из PageFavoritesWrapper

--- src/pages/favorites/wrapper/wrapper.jsx
+++ src/pages/favorites/wrapper/wrapper.jsx
@@ -1,17 +1,11 @@
-import { lazy } from 'react';
 import PropTypes from 'prop-types';
-import ErrorBoundary from '~/components/error-boundary/error-boundary';
-import CommonLayout from '~/layouts/common/common';
-
-const PageFavorites = lazy(() => import('../content/content'));
+import PageFavoritesContent from '../content/content';
 
 const PageFavoritesWrapper = ({ setIsLoading }) => {
   return (
-    <CommonLayout>
-      <ErrorBoundary setIsLoading={setIsLoading}>
-        <PageFavorites />
-      </ErrorBoundary>
-    </CommonLayout>
+    <PageFavoritesContent
+      setIsLoading={setIsLoading}
+    />
   );
 };
 
