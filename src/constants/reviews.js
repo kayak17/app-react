b4fe81d458c256a5399f8785381353b9594ff5b1
kayak17 <kayak93@list.ru>
2022-01-28 23:47:31 +0300

    Добавлен enum ReviewTitles

--- src/constants/reviews.js
+++ src/constants/reviews.js
@@ -3,3 +3,10 @@ export const REVIEW_AVATAR_IMG_SHAPES = {
   width: '54',
   height: '54',
 };
+
+export const ReviewTitles = {
+  NO_REVIEWS: 'No reviews yet',
+  PLACES_NEARBY: 'Other places in the neighbourhood',
+  TO_WRITE_REVIEW: 'To write review please ',
+  YOUR_REVIEW: 'Your review',
+};
