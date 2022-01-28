66a14f44f2761144c1a17d7e9f4479c31a24fb0f
kayak17 <kayak93@list.ru>
2022-01-28 23:49:47 +0300

    В AppMessages добавлены константы INCORRECT_OFFERID и UNAUTHORIZED

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -2,9 +2,11 @@ export const AppMessages = {
   DATA_LOADING_ERROR: 'Data loading error',
   DATA_POSTING_ERROR: 'Data posting error',
   DONT_HAVE_ACCOUNT: ' if you don\'t have an account yet',
+  INCORRECT_OFFERID: 'Incorrect offer id',
   LOGIN_SUCCESS: 'You successfully signed in',
   NO_SUCH_USER: 'User with such email and password is not found',
   PASSWORDS_DONT_MATCH: 'Passwords don\'t match',
   SIGNUP_SUCCESS: 'Signing up completed successfully',
   SOMETHING_WENT_WRONG: 'Something went wrong',
+  UNAUTHORIZED: 'Unauthorized user',
 };
