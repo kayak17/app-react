b778a3ee7438ff5fa6dc74fba350ada29417dc4c
kayak17 <kayak93@list.ru>
2022-01-29 23:14:02 +0300

    Добавлена функция getReviewsURL в utils

--- src/utils/reviews.js
+++ src/utils/reviews.js
@@ -1,7 +1,15 @@
 import format from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
-import { REVIEW_DATE_FORMAT } from '~/constants';
+import {
+  APIRoutes,
+  ReviewsPerPage,
+  REVIEW_DATE_FORMAT,
+} from '~/constants';
 
 export const getReviewDate = (date) => (
   format(parseISO(date), REVIEW_DATE_FORMAT)
+);
+
+export const getReviewsURL = (offerId, pageType) => (
+  `${APIRoutes.REVIEWS}?offerId=${offerId}&_page=1&_limit=${ReviewsPerPage[pageType]}&_sort=date&_order=desc`
 );
