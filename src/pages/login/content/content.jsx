ef235125bbd866fe88233802814b25c55221b0a0
kayak17 <kayak93@list.ru>
2022-01-29 23:03:55 +0300

    Правки использования navigate в FormLogin

--- src/pages/login/content/content.jsx
+++ src/pages/login/content/content.jsx
@@ -1,14 +1,17 @@
 import FormLogin from '~/components/forms/login/login';
+import useRouterNavigate from '~/hooks/use-router-navigate/use-router-navigate';
 import { AppTitles } from '~/constants';
 
 const PageLoginContent = () => {
+  const navigate = useRouterNavigate();
+
   return (
     <section className="page-content-wrapper">
       <h1 className="app-title page-content-title">
         {AppTitles.LOGIN}
       </h1>
       <div className="page-form">
-        <FormLogin />
+        <FormLogin navigate={navigate} />
       </div>
     </section>
   );
