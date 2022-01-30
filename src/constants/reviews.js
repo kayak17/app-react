0d0d7edc3ce02174407696b297fae5f604d8671c
kayak17 <kayak93@list.ru>
2022-01-30 22:28:24 +0300

    В constants/reviews.js добавлены константы для FormReview

--- src/constants/reviews.js
+++ src/constants/reviews.js
@@ -1,14 +1,30 @@
 export const REVIEW_AVATAR_ALT = 'Reviews avatar';
 export const REVIEW_AVATAR_IMG_SHAPES = {
-  width: '54',
-  height: '54',
+  width: 54,
+  height: 54,
+};
+export const REVIEW_COMMENT_LENGTH = 50;
+export const REVIEW_DATE_FORMAT = 'MMMM yyyy';
+export const REVIEW_FORM_STAR_SHAPES = {
+  width: 37,
+  height: 33,
 };
 
-export const REVIEW_DATE_FORMAT = 'MMMM yyyy';
+export const ReviewFormRatings = [
+  { mark: '5', title: 'perfect' },
+  { mark: '4', title: 'good' },
+  { mark: '3', title: 'not bad' },
+  { mark: '2', title: 'badly' },
+  { mark: '1', title: 'terribly' },
+];
 
 export const ReviewTitles = {
   NO_REVIEWS: 'No reviews yet',
+  DESCRIBE_YOUR_STAY: ' and describe your stay with at least ',
   PLACES_NEARBY: 'Other places in the neighbourhood',
+  TEXT_AMOUNT: `${REVIEW_COMMENT_LENGTH} characters`,
+  TEXTAREA_PLACEHOLDER: 'Tell how was your stay, what you like and what can be improved',
+  TO_SUBMIT_REVIEW: 'To submit review please make sure to click ',
   TO_WRITE_REVIEW: 'To write review please ',
   YOUR_REVIEW: 'Your review',
 };
