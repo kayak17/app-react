2cc541ba570233817fdd3e087f66ce4bed771ebc
kayak17 <kayak93@list.ru>
2022-01-30 23:19:27 +0300

    Добавлен компонент PropertyFeaturesPlaceholder

---
+++ src/components/property/features-placeholder/features-placeholder.jsx
@@ -0,0 +1,14 @@
+import '../features/features.less';
+
+const PropertyFeaturesPlaceholder = () => {
+  return (
+    <ul className="d-flex align-items-center justify-content-center
+      list-unstyled property-features placeholder-glow"
+    >
+      <li className="col-1 property-feature placeholder"></li>
+      <li className="col-1 property-feature placeholder"></li>
+    </ul>
+  );
+};
+
+export default PropertyFeaturesPlaceholder;
