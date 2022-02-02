1a80139196e26a04c1f3e7387576f880d7c3cbef
kayak17 <kayak93@list.ru>
2022-02-02 22:44:58 +0300

    Добавлены enums для ButtonBookmark

---
+++ src/constants/bookmark.js
@@ -0,0 +1,43 @@
+export const BookmarkBtnClasses = {
+  MAIN: {
+    btn: 'places-bookmark-button',
+    btnActive: 'bookmark-button-active',
+    icon: 'places-bookmark-icon',
+  },
+  ROOM: {
+    btn: 'property-bookmark-button',
+    btnActive: 'bookmark-button-active',
+    icon: 'property-bookmark-icon',
+  },
+  FAVORITE: {
+    btn: 'places-bookmark-button',
+    btnActive: 'bookmark-button-active',
+    icon: 'places-bookmark-icon',
+  },
+};
+
+export const BookmarkBtnShapes = {
+  MAIN: {
+    width: 18,
+    height: 19,
+  },
+  ROOM: {
+    width: 31,
+    height: 33,
+  },
+  FAVORITE: {
+    width: 18,
+    height: 19,
+  },
+};
+
+export const BookmarkBtnTypes = {
+  FAVORITE: 'FAVORITE',
+  MAIN: 'MAIN',
+  ROOM: 'ROOM',
+};
+
+export const BookmarkBtnTitles = {
+  IN_BOOKMARKS: 'In bookmarks',
+  TO_BOOKMARKS: 'To bookmarks',
+};
