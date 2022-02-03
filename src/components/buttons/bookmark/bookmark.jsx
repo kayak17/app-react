37da05312c0ae651cccfa6529b463153399f2911
kayak17 <kayak93@list.ru>
2022-02-03 23:20:40 +0300

    Обработка данных при нажатии на кнопку в ButtonBookmark

--- src/components/buttons/bookmark/bookmark.jsx
+++ src/components/buttons/bookmark/bookmark.jsx
@@ -1,27 +1,60 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import IconBookmark from '~/icons/bookmark/bookmark';
-import { getIsAuth } from '~/modules/user';
-import { offerIdPropTypes } from '~/prop-types';
+import {
+  getOffersIdsMap,
+  updateOffersIdsMap,
+} from '~/modules/favorites';
+import {
+  getActiveCityId,
+  getActiveCityName,
+} from '~/modules/main';
+import {
+  getIsAuth,
+  getUserId,
+} from '~/modules/user';
+import {
+  offerIdPropTypes,
+} from '~/prop-types';
 import {
   BookmarkBtnClasses,
   BookmarkBtnShapes,
   BookmarkBtnTitles,
   ModalIds,
 } from '~/constants';
+import {
+  getOffersIdsByUserData,
+  getUpdatedOffersIdsMap,
+} from '~/utils';
 
 const ButtonBookmark = ({ offerId, bookmarkType }) => {
+  const dispatch = useDispatch();
+  const activeCityId = useSelector(getActiveCityId);
+  const activeCityName = useSelector(getActiveCityName);
+  const offersIdsMap = useSelector(getOffersIdsMap, shallowEqual);
   const isAuth = useSelector(getIsAuth);
+  const userId = useSelector(getUserId);
 
-  const isInFavorites = false;
+  const [offersIdsByUser, setOffersIdsByUser] = useState(
+    getOffersIdsByUserData(offersIdsMap, userId)
+  );
+
+  const isInFavorites = offersIdsByUser.length ?
+    offersIdsByUser.includes(offerId) : false;
   const btnTitle = isInFavorites ?
     BookmarkBtnTitles.IN_BOOKMARKS : BookmarkBtnTitles.TO_BOOKMARKS
   const loginModalId = isAuth ? ModalIds.NONE : ModalIds.LOGIN;
 
   const handleBookmarkBtnClick = () => {
-    if (isAuth) {
-      console.log(offerId);
+    if (isAuth && userId) {
+      const newOffersIdsMap = getUpdatedOffersIdsMap(
+        offersIdsMap, activeCityId, activeCityName, offerId, userId
+      );
+
+      dispatch(updateOffersIdsMap(newOffersIdsMap));
+      setOffersIdsByUser(getOffersIdsByUserData(newOffersIdsMap, userId));
     }
   };
 
@@ -36,8 +69,8 @@ const ButtonBookmark = ({ offerId, bookmarkType }) => {
         )
       }
       data-modal={loginModalId}
-      type="button"
       title={btnTitle}
+      type="button"
       onClick={handleBookmarkBtnClick}
     >
       <IconBookmark
