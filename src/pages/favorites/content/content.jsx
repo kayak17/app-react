d8eeba98b982f3ae5aff9f7b11c8d8137807605f
kayak17 <kayak93@list.ru>
2022-02-04 21:46:03 +0300

    Добавлена разметка в PageFavoritesContent

--- src/pages/favorites/content/content.jsx
+++ src/pages/favorites/content/content.jsx
@@ -1,7 +1,59 @@
-const PageFavoritesContent = () => {
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import OffersList from '~/components/offer/list/list';
+import { AppTitles, OfferTypes } from '~/constants';
+import { getItemOrNullPropTypes } from '~/prop-types';
+import {
+  getCitiesFromOffersMapByCity,
+  getCityNameByCityId,
+  getLocationLinkByCityId,
+  getOffersByCityId,
+} from '~/utils';
+
+const PageFavoritesContent = ({ isLoaded, offersMapByCity }) => {
   return (
-    <h1>Favorites</h1>
+    <section className="page-content-wrapper">
+      <h1 className="app-title page-content-title">{AppTitles.SAVED_LISTING}</h1>
+      {isLoaded ? (
+        offersMapByCity && offersMapByCity.size ? (
+          <ul className="d-flex flex-column align-items-center list-unstyled">
+            {getCitiesFromOffersMapByCity(offersMapByCity).map((cityId) =>
+              <li key={cityId} className="d-flex">
+                <div className="favorites-link-container">
+                  <div>
+                    <Link
+                      className="btn btn-primary app-skewed-neg-15 fs-5-3 favorites-link"
+                      to={getLocationLinkByCityId(cityId)}
+                    >
+                      <span>{getCityNameByCityId(offersMapByCity, cityId)}</span>
+                    </Link>
+                  </div>
+                </div>
+                <div className="favorites-offers-container">
+                  <OffersList
+                    offers={getOffersByCityId(offersMapByCity, cityId)}
+                    offerType={OfferTypes.FAVORITE}
+                  />
+                </div>
+              </li>
+            )}
+          </ul>
+        ) : (
+          <h2 className="app-subtitle">{AppTitles.NOTHING_SAVED_YET}</h2>
+        )
+      ) : (
+        // placeholder
+        <></>
+      )}
+    </section>
   );
+};
+
+PageFavoritesContent.propTypes = {
+  isLoaded: PropTypes.bool.isRequired,
+  offersMapByCity: getItemOrNullPropTypes(
+    PropTypes.instanceOf(Map).isRequired
+  ),
 };
 
 export default PageFavoritesContent;
