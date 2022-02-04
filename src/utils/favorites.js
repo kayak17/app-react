9e22ceef07c3e410b7afcba13061858d603575d4
kayak17 <kayak93@list.ru>
2022-02-04 20:21:32 +0300

    В utils/favorites убраны ненужные функции и добавлены addOfferToFavorites, removeOfferFromFavorites, getUpdatedFavoriteOffers

--- src/utils/favorites.js
+++ src/utils/favorites.js
@@ -1,67 +1,43 @@
-import isEmpty from 'lodash/isEmpty';
+import union from 'lodash/union';
+import unionBy from 'lodash/unionBy';
 
-export const getUpdatedOffersIdsMap = (
-  data, activeCityId, activeCityName, offerId, userId
-) => {
-  const offersIdsMap = Object.assign({}, data);
+const addOfferToFavorites = ({
+  favoriteOffersIds, favoriteOffersIdsByUser, offerId, userId
+}) => {
+  const offers = favoriteOffersIdsByUser ?
+    union(favoriteOffersIdsByUser, [offerId]) : [offerId];
 
-  if (!isEmpty(offersIdsMap) && userId &&
-    Object.keys(offersIdsMap).includes(userId.toString(10))
-  ) {
-    const dataByUser = offersIdsMap[userId];
-    let dataByCity = undefined;
-    for (const item of dataByUser) {
-      if (item.id === activeCityId) {
-        dataByCity = item;
-        break;
-      }
-    }
-
-    if (dataByCity) {
-      const offerIdsByCity = dataByCity.offersIds ? dataByCity.offersIds : [];
-
-      if (offerIdsByCity.includes(offerId)) {
-        const idx = offerIdsByCity.findIndex((item) => item === offerId);
-        offerIdsByCity.splice(idx, 1);
-      } else {
-        offerIdsByCity.push(offerId);
-      }
-
-      if (!offerIdsByCity.length) {
-        const idx = dataByUser.findIndex((item) => item.id === activeCityId);
-        dataByUser.splice(idx, 1);
-      }
-
-      if (!dataByUser.length) {
-        delete offersIdsMap[userId];
-      }
-    } else {
-      dataByUser.push({
-        id: activeCityId,
-        name: activeCityName,
-        offersIds: [offerId],
-      });
-    }
-  } else {
-    offersIdsMap[userId] = [{
-      id: activeCityId,
-      name: activeCityName,
-      offersIds: [offerId],
-    }];
-  }
-
-  return offersIdsMap;
+  return unionBy([{ userId, offers }], favoriteOffersIds, 'userId');
 };
 
-export const getOffersIdsByUserData = (data, userId) => {
-  const offersIdsMap = Object.assign({}, data);
+const removeOfferFromFavorites = ({
+  favoriteOffersIds, favoriteOffersIdsByUser, offerId, userId
+}) => {
+  const offers = favoriteOffersIdsByUser ?
+    favoriteOffersIdsByUser.filter((item) => item !== offerId) : [];
 
-  if (!isEmpty(offersIdsMap) && userId &&
-    Object.keys(offersIdsMap).includes(userId.toString(10))
-  ) {
-    const dataByUser = offersIdsMap[userId];
-    return dataByUser.reduce((arr, item) => arr.concat(item.offersIds), []);
-  }
-
-  return [];
+  return unionBy([{ userId, offers }], favoriteOffersIds, 'userId');
 };
+
+export const getUpdatedFavoriteOffers = ({
+  isInFavorites,
+  favoriteOffersIds,
+  favoriteOffersIdsByUser,
+  offerId,
+  userId,
+}) => (
+  isInFavorites ?
+    removeOfferFromFavorites({
+      favoriteOffersIds,
+      favoriteOffersIdsByUser,
+      offerId,
+      userId,
+    }) : (
+      addOfferToFavorites({
+        favoriteOffersIds,
+        favoriteOffersIdsByUser,
+        offerId,
+        userId,
+      })
+    )
+);
