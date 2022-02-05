32b13d1c057af8b7cc2dfaad736379f738bfd0d4
kayak17 <kayak93@list.ru>
2022-02-05 23:19:18 +0300

    В PageMainContent добавлена обработка props scrollContainer и setScrolledOffers

--- src/pages/main/content/content.jsx
+++ src/pages/main/content/content.jsx
@@ -4,7 +4,11 @@ import OffersMap from '~/components/offer/map/map';
 import PlacesWrapper from '~/components/places/wrapper/wrapper';
 import useRouterNavigate from '~/hooks/use-router-navigate/use-router-navigate';
 import { AppSRTitles } from '~/constants';
-import { offersPropTypes, offersReducerPropTypes } from '~/prop-types';
+import {
+  offersPropTypes,
+  offersReducerPropTypes,
+  refPropTypes,
+} from '~/prop-types';
 import './content.less';
 
 const PageMainContent = ({
@@ -14,6 +18,8 @@ const PageMainContent = ({
   isOffersLoading,
   isOffersError,
   isOffersLoaded,
+  scrollContainer,
+  setScrolledOffers,
 }) => {
   const redirectToRoute = useRouterNavigate();
 
@@ -23,13 +29,15 @@ const PageMainContent = ({
         <h1 className="visually-hidden">
           {AppSRTitles.MAIN_PAGE_PLACES}
         </h1>
-        <section className="col-6 overflow-auto main-places-container">
+        <section className="col-6 g-0 d-flex flex-column text-center main-places-container">
           <PlacesWrapper
             offersReducer={offersReducer}
             isCitiesError={isCitiesError}
             isOffersError={isOffersError}
             isOffersLoading={isOffersLoading}
             isOffersLoaded={isOffersLoaded}
+            scrollContainer={scrollContainer}
+            setScrolledOffers={setScrolledOffers}
           />
         </section>
         <section className="col-6 g-0 main-map-container">
@@ -50,6 +58,8 @@ PageMainContent.propTypes = {
   isOffersLoading: PropTypes.bool.isRequired,
   isOffersError: PropTypes.bool.isRequired,
   isOffersLoaded: PropTypes.bool.isRequired,
+  scrollContainer: refPropTypes,
+  setScrolledOffers: PropTypes.func.isRequired,
 };
 
 export default memo(PageMainContent);
