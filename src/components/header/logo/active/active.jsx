d9cb83203517a10693cf5954b117984be5b16229
kayak17 <kayak93@list.ru>
2022-02-12 10:23:16 +0300

    Правки стиля кода в HeaderLogoActive

--- src/components/header/logo/active/active.jsx
+++ src/components/header/logo/active/active.jsx
@@ -4,7 +4,8 @@ import { AppRoutes, AppTitles } from '~/constants';
 const HeaderLogoActive = () => {
   return (
     <Link
-      className="app-subtitle d-flex align-items-center mb-0 me-auto text-dark text-decoration-none"
+      className="app-subtitle d-flex align-items-center mb-0 me-auto
+        text-dark text-decoration-none"
       to={AppRoutes.MAIN}
     >
       <span className="fs-4">{AppTitles.LOGO_TITLE}</span>
