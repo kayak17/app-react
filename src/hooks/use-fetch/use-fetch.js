0d723cf86f9b6b237e4c4b12730e90c1c6d3b39d
kayak17 <kayak93@list.ru>
2022-02-12 09:58:28 +0300

    Правки описания JSDoc для useFetch

--- src/hooks/use-fetch/use-fetch.js
+++ src/hooks/use-fetch/use-fetch.js
@@ -10,7 +10,7 @@ import {
 import { getUnknownActionTypeMsg } from '~/utils';
 
 /**
- * Object with data from server request
+ * Object with data from server response
  * @typedef {Object} payloadObject
  * @property {array} data - data from server response
  * @property {Object} headerLink - link header from server response
@@ -37,7 +37,7 @@ import { getUnknownActionTypeMsg } from '~/utils';
 /**
  * Callback to run if request was successful
  * @callback onSuccessCallback
- * @param {payloadObject} payload - contains data from server request
+ * @param {payloadObject} payload - contains data from server response
  */
 
 /**
