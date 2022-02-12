6c290728eedd84d19f2fad3796d06a98c925ab05
kayak17 <kayak93@list.ru>
2022-02-12 10:13:17 +0300

    Формирование роутов вынесено из App в RoutesApp

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,51 +1,14 @@
 import { compose } from 'redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import PrivateRoute from '../private-route/private-route';
+import { BrowserRouter } from 'react-router-dom';
 import withLazy from '~/hocs/with-lazy/with-lazy';
 import withModal from '~/hocs/with-modal/with-modal';
 import withToaster from '~/hocs/with-toaster/with-toaster';
-import PageFavorites from '~/pages/favorites/container/container';
-import PageLogin from '~/pages/login/container/container';
-import PageMain from '~/pages/main/container/container';
-import PageNotFound from '~/pages/not-found/container/container';
-import PageSignUp from '~/pages/sign-up/container/container';
-import PageRoom from '~/pages/room/container/container';
-import { AppRoutes } from '~/constants';
+import RoutesApp from '../routes-app/routes-app';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoutes.MAIN}
-          element={<PageMain />}
-        />
-        <Route
-          path={AppRoutes.FAVORITES}
-          element={
-            <PrivateRoute
-              redirectURL={AppRoutes.LOGIN}
-              element={<PageFavorites />}
-            />
-          }
-        />
-        <Route
-          path={AppRoutes.LOGIN}
-          element={<PageLogin />}
-        />
-        <Route
-          path={AppRoutes.SIGNUP}
-          element={<PageSignUp />}
-        />
-        <Route
-          path={AppRoutes.OFFER}
-          element={<PageRoom />}
-        />
-        <Route
-          path="*"
-          element={<PageNotFound />}
-        />
-      </Routes>
+      <RoutesApp />
     </BrowserRouter>
   );
 };
