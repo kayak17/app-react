46a2c43b851cda8b4518b66ac7c333c68216398a
kayak17 <kayak93@list.ru>
2022-02-12 09:16:03 +0300

    Добавлены offerMapPropTypes и offersMapPropTypes

--- src/prop-types/offers-map.js
+++ src/prop-types/offers-map.js
@@ -1,3 +1,18 @@
+import PropTypes from 'prop-types';
 import { offerIdPropTypes } from './offer';
 
 export const mapPinIdPropTypes = offerIdPropTypes;
+
+export const offerMapPropTypes = PropTypes.shape({
+  city: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+  coordinates: PropTypes.arrayOf(PropTypes.number).isRequired,
+  id: offerIdPropTypes,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+}).isRequired;
+
+export const offersMapPropTypes = PropTypes.arrayOf(offerMapPropTypes).isRequired;
