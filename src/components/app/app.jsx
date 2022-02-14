87eb895fad27b8af70e04b66aeaa64f8f02efc14
kayak17 <kayak93@list.ru>
2022-02-14 11:36:34 +0300

    Рефакторинг App при помощи useRoutes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,49 @@
 import { compose } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import withLazy from '~/hocs/with-lazy/with-lazy';
 import withModal from '~/hocs/with-modal/with-modal';
 import withToaster from '~/hocs/with-toaster/with-toaster';
-import RoutesApp from '../routes-app/routes-app';
+import PageFavorites from '~/pages/favorites/container/container';
+import PageLogin from '~/pages/login/container/container';
+import PageMain from '~/pages/main/container/container';
+import PageNotFound from '~/pages/not-found/container/container';
+import PageSignUp from '~/pages/sign-up/container/container';
+import PageRoom from '~/pages/room/container/container';
+import PrivateRoute from '../private-route/private-route';
+import { AppRoutes } from '~/constants';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <RoutesApp />
-    </BrowserRouter>
+    useRoutes([
+      {
+        path: AppRoutes.MAIN,
+        element: <PageMain />,
+      },
+      {
+        path: AppRoutes.FAVORITES,
+        element:
+          <PrivateRoute
+            redirectURL={AppRoutes.LOGIN}
+            element={<PageFavorites />}
+          />
+      },
+      {
+        path: AppRoutes.LOGIN,
+        element: <PageLogin />,
+      },
+      {
+        path: AppRoutes.SIGNUP,
+        element: <PageSignUp />,
+      },
+      {
+        path: AppRoutes.OFFER,
+        element: <PageRoom />,
+      },
+      {
+        path: AppRoutes.ANY,
+        element: <PageNotFound />,
+      },
+    ])
   );
 };
 
