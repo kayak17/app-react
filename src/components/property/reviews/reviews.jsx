7e1794628fbb772aea62007961ce4a73ff80da6d
kayak17 <kayak93@list.ru>
2022-02-14 15:48:23 +0300

    Рефакторинг PropertyReviews

--- src/components/property/reviews/reviews.jsx
+++ src/components/property/reviews/reviews.jsx
@@ -1,19 +1,13 @@
-import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import FormReview from '~/components/forms/review/review';
 import ReviewsList from '~/components/review/list/list';
-import ReviewsNoAuth from '~/components/review/no-auth/no-auth';
-import { getIsAuth } from '~/modules/user';
 import { AppTitles, ReviewTitles } from '~/constants';
 import { reviewsPropTypes } from '~/prop-types';
 
 const PropertyReviews = ({
-  offerId,
   reviews,
   reviewsCount,
   fetchReviews,
 }) => {
-  const isAuth = useSelector(getIsAuth);
   const canShowLoadMoreBtn = reviews.length < parseInt(reviewsCount, 10);
 
   const onLoadMoreBtnClick = () => {
@@ -22,48 +16,36 @@ const PropertyReviews = ({
     }
   };
 
-  return (
-    <section>
-      {reviews.length ? (
-        <>
-          <h2 className="app-subtitle mb-4">
-            {AppTitles.REVIEWS}
-            &nbsp;&middot;&nbsp;
-            <span>{reviewsCount}</span>
-          </h2>
-          <ReviewsList reviews={reviews} />
-          {canShowLoadMoreBtn && (
-            <div className="mb-3">
-              <button
-                className="btn btn-primary"
-                type="button"
-                onClick={onLoadMoreBtnClick}
-              >
-                {AppTitles.LOAD_MORE}
-              </button>
-            </div>
-          )}
-        </>
-      ) : (
-        <h2 className="fs-3 app-subtitle">{ReviewTitles.NO_REVIEWS}</h2>
-      )}
+  if (reviews.length) {
+    return (
+      <>
+        <h2 className="app-subtitle mb-4">
+          {AppTitles.REVIEWS}
+          &nbsp;&middot;&nbsp;
+          <span>{reviewsCount}</span>
+        </h2>
+        <ReviewsList reviews={reviews} />
+        {canShowLoadMoreBtn && (
+          <div className="mb-3">
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={onLoadMoreBtnClick}
+            >
+              {AppTitles.LOAD_MORE}
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
 
-      {isAuth ? (
-        <>
-          <p className="text-start fw-bold fst-italic">
-            {ReviewTitles.YOUR_REVIEW}
-          </p>
-          <FormReview offerId={offerId} />
-        </>
-      ) : (
-        <ReviewsNoAuth />
-      )}
-    </section>
+  return (
+    <h2 className="fs-3 app-subtitle">{ReviewTitles.NO_REVIEWS}</h2>
   );
 };
 
 PropertyReviews.propTypes = {
-  offerId: PropTypes.number.isRequired,
   reviews: reviewsPropTypes,
   reviewsCount: PropTypes.string.isRequired,
   fetchReviews: PropTypes.func.isRequired,
