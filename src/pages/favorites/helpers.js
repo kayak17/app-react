1c4de52c2bf48e3c319a5db63891d56b6b6134cb
kayak17 <kayak93@list.ru>
2022-02-14 15:44:04 +0300

    Убраны лишние функции из pages/favorites/helpers

--- src/pages/favorites/helpers.js
+++ src/pages/favorites/helpers.js
@@ -41,15 +41,3 @@ export const getUpdatedOffersMap = (offersMap, offersIds) => {
 
   return offersMap;
 };
-
-export const getCitiesFromOffersMapByCity = (offersMapByCity) => (
-  Array.from(offersMapByCity.keys())
-);
-
-export const getCityNameByCityId = (offersMapByCity, cityId) => (
-  offersMapByCity.get(cityId)[0].city.name
-);
-
-export const getOffersByCityId = (offersMapByCity, cityId) => (
-  offersMapByCity.get(cityId)
-);
