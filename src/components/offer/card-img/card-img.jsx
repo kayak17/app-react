06ce16c6b9005350312bdf522f729c72425f336a
kayak17 <kayak93@list.ru>
2022-02-14 16:36:48 +0300

    Добавлен компонент OfferCardImg

---
+++ src/components/offer/card-img/card-img.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { OfferImgShapes, OfferTitles } from '~/constants';
+
+const OfferCardImg = ({
+  offerImage,
+  offerLink,
+  offerType,
+}) => {
+  return (
+    <NavLink
+      className="d-flex justify-content-center align-items-center text-center"
+      to={offerLink}
+    >
+      <img
+        className="rounded"
+        alt={OfferTitles.PLACE_IMAGE}
+        width={OfferImgShapes[offerType].width}
+        height={OfferImgShapes[offerType].height}
+        src={offerImage}
+        loading="lazy"
+      />
+    </NavLink>
+  );
+
+};
+
+OfferCardImg.propTypes = {
+  offerImage: PropTypes.string.isRequired,
+  offerLink: PropTypes.string.isRequired,
+  offerType: PropTypes.string.isRequired,
+};
+
+export default OfferCardImg;
