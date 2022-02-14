39108c9126d3ec72b66092b5b72bcfb97e3e9bb7
kayak17 <kayak93@list.ru>
2022-02-14 15:44:39 +0300

    Добавлен компонент FavoritesContentPlaceholder

---
+++ src/components/favorites/content-placeholder/content-placeholder.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import OffersListPlaceholder from '~/components/offer/list-placeholder/list-placeholder';
+import { getArrayOfUndefineds } from '~/utils';
+import './favorites.less';
+
+const FavoritesContentPlaceholder = ({ offerType }) => {
+  const dataArray = getArrayOfUndefineds(3);
+
+  return (
+    <ul
+      className="d-flex flex-column align-items-center
+        list-unstyled placeholder-glow"
+      aria-hidden="true"
+    >
+      <li className="d-flex">
+        <div className="favorites-link-container">
+          <div>
+            <a
+              className="col-6 btn btn-primary app-skewed-neg-15 fs-5-3
+                disabled placeholder favorites-link"
+              tabIndex="-1"
+            >
+            </a>
+          </div>
+        </div>
+        <div className="favorites-offers-container">
+          <OffersListPlaceholder
+            offers={dataArray}
+            offerType={offerType}
+          />
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+FavoritesContentPlaceholder.propTypes = {
+  offerType: PropTypes.string.isRequired,
+};
+
+export default FavoritesContentPlaceholder;
