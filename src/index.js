5b7c45cf95e60fddd1490a36a9f195ea28fa8df5
kayak17 <kayak93@list.ru>
2022-02-20 21:24:26 +0300

    Компонент Toaster перенесен в index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Toaster } from 'react-hot-toast';
 import { persistor, store } from './store';
 import App from './components/app/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +13,7 @@ render(
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
+        <Toaster />
       </BrowserRouter>
     </PersistGate>
   </Provider>,
