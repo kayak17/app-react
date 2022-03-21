b4cdbe3d99e428092375d13ad00339df4be9b6d5
kayak17 <kayak93@list.ru>
2022-03-21 10:32:21 +0300

    Оптимизация PageFavoritesContent

--- src/pages/favorites/content/content.jsx
+++ src/pages/favorites/content/content.jsx
@@ -1,12 +1,12 @@
+import isEqual from 'lodash/isEqual';
 import PropTypes from 'prop-types';
 import FavoritesContent from '~/components/favorites/content/content';
 import FavoritesContentPlaceholder from '~/components/favorites/content-placeholder/content-placeholder';
 import { AppTitles, OfferTypes } from '~/constants';
 import { offersMapByCityPropTypes } from '~/prop-types';
+import { memo } from 'react';
 
-const PageFavoritesContent = ({ isLoaded, offerIdsLength, offersMapByCity }) => {
-  const canShowPlaceholder = Boolean(offerIdsLength);
-
+const PageFavoritesContent = ({ isLoaded, offersMapByCity }) => {
   if (isLoaded) {
     if (offersMapByCity && offersMapByCity.size) {
       return (
@@ -17,23 +17,27 @@ const PageFavoritesContent = ({ isLoaded, offerIdsLength, offersMapByCity }) => 
     }
 
     return (
-      <h2 className="app-subtitle">{AppTitles.NOTHING_SAVED_YET}</h2>
-    );
-  } else if (canShowPlaceholder) {
-    return (
-      <FavoritesContentPlaceholder
-        offerType={OfferTypes.FAVORITE}
-      />
+      <h2 className="app-subtitle">
+        {AppTitles.NOTHING_SAVED_YET}
+      </h2>
     );
   }
 
-  return null;
+  return (
+    <FavoritesContentPlaceholder
+      offerType={OfferTypes.FAVORITE}
+    />
+  );
 };
 
 PageFavoritesContent.propTypes = {
   isLoaded: PropTypes.bool.isRequired,
-  offerIdsLength: PropTypes.number.isRequired,
   offersMapByCity: offersMapByCityPropTypes,
 };
 
-export default PageFavoritesContent;
+const arePropsEqual = (prevProps, newProps) => (
+  prevProps.isLoaded === newProps.isLoaded &&
+  isEqual(prevProps.offersMapByCity, newProps.offersMapByCity)
+);
+
+export default memo(PageFavoritesContent, arePropsEqual);
