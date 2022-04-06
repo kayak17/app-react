968e2e9f24faaedf1227d9791d044b537f408738
kayak17 <kayak93@list.ru>
2022-04-06 13:27:03 +0300

    Убран код для переноса в другие компоненты из PageMainWrapper

--- src/pages/main/wrapper/wrapper.jsx
+++ src/pages/main/wrapper/wrapper.jsx
@@ -1,36 +1,15 @@
 import find from 'lodash/find';
-import isEmpty from 'lodash/isEmpty';
-import { createContext, useCallback, useEffect, useReducer, useRef, useState } from 'react';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import CitiesWrapper from '~/components/cities/wrapper/wrapper';
-import PageMainContent from '../content/content';
+import PageMainWrapperOffers from '../wrapper-offers/wrapper-offers';
 import useFetchCached from '~/hooks/use-fetch-cached/use-fetch-cached';
-import usePrevious from '~/hooks/use-previous/use-previous';
-import {
-  getActiveCityId,
-  getActiveCityName,
-  getSortingType,
-  setActiveCity,
-} from '~/modules/main';
-import {
-  APIRoutes,
-  AppActionTypes,
-} from '~/constants';
-import {
-  appScrollTo,
-  getOffersURL,
-  getOffersMapURL,
-  throwUnknownActionError,
-} from '~/utils';
-
-export const ScrolledOffersContext = createContext(null);
-export const ScrollContainerContext = createContext(null);
-export const ScrolledOffersDispatchContext = createContext(null);
+import { getActiveCityId, setActiveCity } from '~/modules/main';
+import { APIRoutes } from '~/constants';
 
 const PageMainWrapper = ({ setIsLoading }) => {
-  const COMPONENT_NAME = 'PageMainWrapper';
   const dispatch = useDispatch();
   const location = useLocation();
 
@@ -38,58 +17,7 @@ const PageMainWrapper = ({ setIsLoading }) => {
   const cityId = params.get('city.id');
 
   const activeCityId = useSelector(getActiveCityId);
-  const activeCityName = useSelector(getActiveCityName);
-  const sortingType = useSelector(getSortingType);
   const [cities, setCities] = useState([]);
-  const [offersMap, setOffersMap] = useState([]);
-  const scrollContainer = useRef(null);
-
-  const offersURL = getOffersURL(activeCityId, sortingType);
-  const prevOffersURL = usePrevious(offersURL);
-  const offersMapURL = getOffersMapURL(activeCityId);
-  const prevOffersMapURL = usePrevious(offersMapURL);
-
-  const initialState = {
-    activeCityName: '',
-    data: [],
-    headerLink: {},
-    totalCount: '',
-  };
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case AppActionTypes.SET_DATA:
-        return {
-          ...state,
-          activeCityName: action.payload.activeCityName,
-          data: action.payload.data,
-          headerLink: action.payload.headerLink,
-          totalCount: action.payload.totalCount,
-        };
-      case AppActionTypes.SET_SCROLLED_DATA:
-        return {
-          ...state,
-          data: action.payload.data,
-          headerLink: action.payload.headerLink,
-        };
-      default:
-        throwUnknownActionError(COMPONENT_NAME);
-    }
-  };
-
-  const [offersReducer, dispatchData] = useReducer(reducer, initialState);
-
-  const setOffersData = useCallback((payload) => {
-    dispatchData({
-      type: AppActionTypes.SET_DATA,
-      payload: {
-        activeCityName,
-        data: payload.data,
-        headerLink: payload.headerLink,
-        totalCount: payload.totalCount,
-      },
-    });
-  }, [activeCityName]);
 
   const {
     isError: isCitiesError,
@@ -106,89 +34,13 @@ const PageMainWrapper = ({ setIsLoading }) => {
       const newActiveCity = cityId ?
         find(cities, ['id', cityId]) || cities[0] :
         find(cities, ['id', activeCityId]) || cities[0];
-      const newActiveCityId = newActiveCity.id;
 
       dispatch(setActiveCity(newActiveCity));
-      fetchOffers(getOffersURL(newActiveCityId, sortingType));
-      fetchMapOffers(getOffersMapURL(newActiveCityId));
     },
     onFail: () => {
       setIsLoading(false);
     },
   });
-
-  const {
-    cache: cacheoffers,
-    isError: isOffersError,
-    isLoaded: isOffersLoaded,
-    fetchData: fetchOffers,
-  } = useFetchCached({
-    onSuccess: (payload) => {
-      setOffersData(payload);
-      setIsLoading(false);
-    },
-    onFail: () => {
-      setIsLoading(false);
-    },
-  });
-
-  const {
-    cache: cacheoffersMap,
-    isLoaded: isOffersMapLoaded,
-    fetchData: fetchMapOffers,
-  } = useFetchCached({
-    onSuccess: (payload) => {
-      setOffersMap(payload.data);
-    },
-  });
-
-  useEffect(() => {
-    if (
-      isCitiesLoaded && isOffersLoaded &&
-      prevOffersURL !== offersURL
-    ) {
-      appScrollTo(scrollContainer);
-      const payload = cacheoffers.current[offersURL];
-
-      if (isEmpty(payload)) {
-        setIsLoading(true);
-        fetchOffers(offersURL);
-      } else {
-        setOffersData(payload);
-      }
-    }
-  }, [
-    cacheoffers,
-    fetchOffers,
-    setOffersData,
-    isCitiesLoaded,
-    isOffersLoaded,
-    offersURL,
-    prevOffersURL,
-    setIsLoading,
-  ]);
-
-  useEffect(() => {
-    if (
-      isCitiesLoaded && isOffersMapLoaded &&
-      prevOffersMapURL !== offersMapURL
-    ) {
-      const payload = cacheoffersMap.current[offersMapURL];
-
-      if (isEmpty(payload)) {
-        fetchMapOffers(offersMapURL);
-      } else {
-        setOffersMap(payload.data);
-      }
-    }
-  }, [
-    cacheoffersMap,
-    fetchMapOffers,
-    isCitiesLoaded,
-    isOffersMapLoaded,
-    offersMapURL,
-    prevOffersMapURL,
-  ]);
 
   return (
     <>
@@ -197,17 +49,11 @@ const PageMainWrapper = ({ setIsLoading }) => {
         isCitiesError={isCitiesError}
         isCitiesLoaded={isCitiesLoaded}
       />
-      <ScrolledOffersContext.Provider value={offersReducer}>
-        <ScrollContainerContext.Provider value={scrollContainer}>
-          <ScrolledOffersDispatchContext.Provider value={dispatchData}>
-            <PageMainContent
-              offersMap={offersMap}
-              isOffersError={isOffersError}
-              isOffersLoaded={isOffersLoaded}
-            />
-          </ScrolledOffersDispatchContext.Provider>
-        </ScrollContainerContext.Provider>
-      </ScrolledOffersContext.Provider>
+      {isCitiesLoaded && (
+        <PageMainWrapperOffers
+          setIsLoading={setIsLoading}
+        />
+      )}
     </>
   );
 };
