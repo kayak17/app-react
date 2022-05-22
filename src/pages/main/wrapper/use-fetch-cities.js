7f40dd75dfab12133eb76d5c6f806a6585a002a1
kayak17 <kayak93@list.ru>
2022-05-22 22:59:43 +0300

    Добавлен кастомный хук useFetchCities

---
+++ src/pages/main/wrapper/use-fetch-cities.js
@@ -0,0 +1,57 @@
+import find from 'lodash/find';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import useFetchCached from '~/hooks/use-fetch-cached/use-fetch-cached';
+import { getActiveCityId, setActiveCity } from '~/modules/main';
+import { APIRoutes } from '~/constants';
+
+const useFetchCities = (props) => {
+  const HOOK_NAME = 'useFetchCities';
+
+  const propTypes = {
+    setCities: PropTypes.func.isRequired,
+    setIsLoading: PropTypes.func.isRequired,
+  };
+
+  PropTypes.checkPropTypes(propTypes, props, 'prop', HOOK_NAME);
+
+  const {
+    setCities,
+    setIsLoading,
+  } = props;
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const params = new URLSearchParams(location.search);
+  const cityId = params.get('city.id');
+  const activeCityId = useSelector(getActiveCityId);
+
+  const {
+    isError: isCitiesError,
+    isLoaded: isCitiesLoaded,
+  } = useFetchCached({
+    url: APIRoutes.CITIES,
+    onRequest: () => {
+      setIsLoading(true);
+    },
+    onSuccess: (payload) => {
+      const cities = payload.data;
+      setCities(cities);
+
+      const newActiveCity = cityId ?
+        find(cities, ['id', cityId]) || cities[0] :
+        find(cities, ['id', activeCityId]) || cities[0];
+
+      dispatch(setActiveCity(newActiveCity));
+    },
+    onError: () => {
+      setIsLoading(false);
+    },
+  });
+
+  return { isCitiesError, isCitiesLoaded };
+};
+
+export default useFetchCities;
