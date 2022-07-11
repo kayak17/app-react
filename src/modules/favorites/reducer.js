72d8bc6cdb25bec4c7f96ca10f00a6160f9c32f6
kayak17 <kayak93@list.ru>
2022-07-11 08:39:22 +0300

    Рефакторинг modules/favorites/reducer

--- src/modules/favorites/reducer.js
+++ src/modules/favorites/reducer.js
@@ -1,15 +1,55 @@
-import { handleActions } from 'redux-actions';
-import { combineReducers } from 'redux';
-import { setFavoriteOffersIds } from './actions';
-import { InitialValues } from './constants';
+import { ActionTypes, InitialValues } from './constants';
 
-const favoriteOffersIds = handleActions(
-  {
-    [setFavoriteOffersIds]: (_state, action) => action.payload,
-  },
-  InitialValues.FAVORITE_OFFERS_IDS
-);
+const initialState = {
+  favoritesIdsAllUsers: InitialValues.FAVORITES_IDS_ALL_USERS,
+  favoritesIds: InitialValues.FAVORITES_IDS,
+  favorites: InitialValues.FAVORITES,
+  isLoaded: InitialValues.IS_LOADED,
+  errorMsg: InitialValues.ERROR_MSG,
+};
 
-export default combineReducers({
-  favoriteOffersIds,
-});
+const favoritesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.SET_FAVORITES_REQUEST:
+      return {
+        ...state,
+        isLoaded: InitialValues.IS_LOADED,
+        errorMsg: InitialValues.ERROR_MSG,
+      };
+
+    case ActionTypes.SET_FAVORITES_SUCCESS:
+      return {
+        ...state,
+        favorites: action.payload,
+        isLoaded: true,
+        errorMsg: null,
+      };
+
+    case ActionTypes.SET_FAVORITES_ERROR:
+      return {
+        ...state,
+        isLoaded: false,
+        errorMsg: action.payload,
+      };
+
+    case ActionTypes.ADD_TO_FAVORITES_IDS:
+      return {
+        ...state,
+        favoritesIdsAllUsers: action.payload.favoritesIdsAllUsers,
+        favoritesIds: action.payload.favoritesIds,
+      };
+
+    case ActionTypes.REMOVE_FROM_FAVORITES:
+      return {
+        ...state,
+        favoritesIdsAllUsers: action.payload.favoritesIdsAllUsers,
+        favoritesIds: action.payload.favoritesIds,
+        favorites: action.payload.favorites,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default favoritesReducer;
